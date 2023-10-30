bdc9c57ca77554b2c8fedbf1d4f27ee543e56330
Marco Barnfield <eh19374@bc4login2.bc4.acrc.priv>
2023-10-30 17:13:25 +0000

    cython initial

---
+++ code/cython/setup_LebwohlLasher_cython.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+from Cython.Build import cythonize
+from distutils.extension import Extension
+import numpy
+
+
+LebwohlLasher = Extension(
+    "LebwohlLasher",
+    sources=["./code/cython/LebwohlLasher_cython.pyx"],
+    extra_compile_args=['-O3'],
+    extra_link_args=['-O3']
+)
+
+setup(
+    ext_modules = cythonize(LebwohlLasher),
+    include_dirs=[numpy.get_include()],  # Include NumPy header files
+    packages=find_packages(),
+)
